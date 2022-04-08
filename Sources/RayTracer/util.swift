8c88390f7d2e2cee28ae41f53a39b2b12fc532f1
Mukul Agarwal <agarmukul23@gmail.com>
2022-04-08 01:44:16 -0500

    add utility functions

--- Sources/RayTracer/util.swift
+++ Sources/RayTracer/util.swift
@@ -1,8 +1,17 @@
 //
-//  File.swift
+//  util.swift
 //  
 //
 //  Created by Mukul Agarwal on 4/8/22.
 //
 
 import Foundation
+
+
+func degreesToRadians(_ x: Double) -> Double {
+    return x * Double.pi / 180.0
+}
+
+func radiansToDegrees(_ x: Double) -> Double {
+    return x * 180.0 / Double.pi
+}
