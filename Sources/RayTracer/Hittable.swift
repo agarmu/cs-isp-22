2b085d08d1bed412899c2072f5bc04f49c8a8718
Mukul Agarwal <agarmukul23@gmail.com>
2022-04-08 00:58:48 -0500

    add Hittable system

---
+++ Sources/RayTracer/Hittable.swift
@@ -0,0 +1,34 @@
+//
+//  Hittable.swift
+//  
+//
+//  Created by Mukul Agarwal on 4/7/22.
+//
+
+import Foundation
+
+struct HitRecord {
+    let p: Point
+    let normal: Vector
+    let t: Double
+    let frontFace: Bool
+    let object: Hittable
+    init(p: Point, normal: Vector, t: Double, frontFace: Bool, object: Hittable) {
+        self.p = p
+        self.normal = normal
+        self.t = t
+        self.frontFace = frontFace
+        self.object = object
+    }
+    init(ray: Ray, t: Double, outwardNormal: Vector, object: Hittable) {
+        self.p = ray[t]
+        self.frontFace = (outwardNormal • ray.direction) < 0
+        normal = (frontFace ? 1: -1) * outwardNormal
+        self.t = t
+        self.object = object
+    }
+}
+
+protocol Hittable {
+    func hit(ray: Ray, time: ClosedRange<Double>) -> HitRecord?
+}
