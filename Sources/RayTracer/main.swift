640f16a7555128291669cd5039a63a5db421e6e7
Mukul Agarwal <agarmukul23@gmail.com>
2022-04-09 15:02:09 -0500

    fix objects

--- Sources/RayTracer/main.swift
+++ Sources/RayTracer/main.swift
@@ -5,11 +5,19 @@ let scene = Scene(
     width: 400,
     aspectRatio: 16/9,
     objects: [
-		Sphere([0,0,-1.5], 0.5, mat: Lambertian([1, 0, 0])),
-		Sphere([0,-100.5,-1.5], 100, mat: Lambertian([0.8, 0.8, 0])),
-		Sphere([1.0, 0, -1.5], 0.45, mat: Dielectric(indexOfRefraction: 2/3)),
-		Sphere([1.0, 0, -1.5], 0.5, mat: Dielectric(indexOfRefraction: 3/2)),
-		Sphere([-1.0, 0, -1.5], 0.5, mat: Metal(albedo: [0.2, 0.2, 0.2], fuzz: 0))
+		// ground
+		Sphere([0,-100.5,-1], 100, mat: Lambertian([0.8, 0.8, 0])),
+
+		//
+		Sphere([0,0,-1], 0.5, mat: Lambertian([0.1, 0.2, 0.5])),
+		
+		// glass sphere - outer
+		Sphere([-1.0, 0, -1], 0.5, mat: Dielectric(indexOfRefraction: 3/2)),
+		// glass sphere - hollow inner
+		Sphere([-1.0, 0, -1], 0.45, mat: Dielectric(indexOfRefraction: 2/3)),
+		
+		// metal sphere
+		Sphere([1.0, 0, -1], 0.5, mat: Metal(albedo: [0.8, 0.6, 0.2], fuzz: 0))
     ]
 )
 scene.scan()
