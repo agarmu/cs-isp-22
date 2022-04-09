448de61c275aab62bde8b970d015ce5838859376
Mukul Agarwal <agarmukul23@gmail.com>
2022-04-09 17:01:15 -0500

    create part 1 final rendering

--- Sources/RayTracer/main.swift
+++ Sources/RayTracer/main.swift
@@ -2,27 +2,56 @@ import Foundation
 
 print("starting")
 let scene = Scene(
-    width: 400,
-    aspectRatio: 16/9,
-	cam: Camera(lookFrom: [3, 3, 2], lookAt: [0, 0, -1], vUp: Vector(0, 1, 0).normalized(), vFov: 20, aspectRatio: 16.0/9.0, aperture: 2.0),
-    objects: [
-		// ground
-		Sphere([0,-100.5,-1], 100, mat: Lambertian([0.8, 0.8, 0])),
-
-		//
-		Sphere([0,0,-1], 0.5, mat: Lambertian([0.1, 0.2, 0.5])),
-		
-		// glass sphere - outer
-		Sphere([-1.0, 0, -1], 0.5, mat: Dielectric(indexOfRefraction: 3/2)),
-		// glass sphere - hollow inner
-		Sphere([-1.0, 0, -1], 0.45, mat: Dielectric(indexOfRefraction: 2/3)),
-		
-		// metal sphere
-		Sphere([1.0, 0, -1], 0.5, mat: Metal(albedo: [0.8, 0.6, 0.2], fuzz: 0))
-    ]
+    width: 1200,
+    aspectRatio: 3/2,
+	cam: Camera(lookFrom: [13, 2, 3], lookAt: [0, 0, 0], vUp: Vector(0, 1, 0), vFov: 20, aspectRatio: 3/2, aperture: 0.1, focalDistance: 10),
+    objects: generateObjects()
 )
 scene.scan()
 scene.export()
 
 
 
+func generateObjects() -> [Hittable] {
+	let ground = Lambertian([0.5, 0.5, 0.5])
+	let glass = Dielectric(indexOfRefraction: 3/2)
+	let glassInner = Dielectric(indexOfRefraction: 2/3)
+	var objects: [Hittable] = (-11..<11).flatMap { a in
+		(-11..<11).map { b in
+			let material: Material
+			let center = Point(
+				Double(a) + Double.random(in: 0...0.9),
+				0.2,
+				Double(b) + Double.random(in: 0...0.9)
+			)
+			// determine material
+			let d = Double.random(in: 0...1)
+			if d < 0.8 {
+				// diffuse
+				let albedo = Color.random(in: 0...1) ⊙ Color.random(in: 0...1)
+				material = Lambertian(albedo)
+			} else if d < 0.95 {
+				// metal
+				let fuzz = Double.random(in: 0...0.5)
+				let albedo = Color.random(in: 0.5...1)
+				material = Metal(albedo: albedo, fuzz: fuzz)
+			} else {
+				// dielectric
+				material = glass
+			}
+			return Sphere(center, 0.2, mat: material)
+		}
+	}
+	objects.append(contentsOf: [
+		Sphere([0, -1000, 0], 1000, mat: ground),
+		// three large spheres!
+		//  	metal
+		Sphere([4,1,0], 1, mat: Metal(albedo: [0.7, 0.6, 0.5], fuzz: 0)),
+		//   	hollow glass
+		Sphere([0, 1, 0], 1, mat: glass), // outer
+		Sphere([0, 1, 0], 0.9, mat: glassInner), // inner
+		//   	lambertian - brown
+		Sphere([-4, 1, 0], 1, mat: Lambertian([0.4, 0.2, 0.1]))
+	])
+	return objects
+}
