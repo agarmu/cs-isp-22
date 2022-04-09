eb2dbb5619c2f528b76e3837cfa793db9509ed35
Mukul Agarwal <agarmukul23@gmail.com>
2022-04-09 10:06:15 -0500

    add more spheres to demonstrate different materials

--- Sources/RayTracer/main.swift
+++ Sources/RayTracer/main.swift
@@ -7,9 +7,9 @@ let scene = Scene(
     objects: [
 		Sphere([0,0,-1.5], 0.5, mat: Lambertian(albedo: [1, 0, 0])),
 		Sphere([0,-100.5,-1.5], 100, mat: Lambertian(albedo: [0.8, 0.8, 0])),
-		Sphere([-1.0, 0, -1.5], 0.5, mat: Metal(albedo: [0.2, 0.2, 0.2], fuzz: 0)),
+		Sphere([-1.0, 0, -1.5], 0.5, mat: Dielectric(indexOfRefraction: 2/3)),
 		Sphere([1.0, 0, -1.5], 0.5, mat: Dielectric(indexOfRefraction: 3/2)),
-		Sphere([0, 0, -.5], 0.17, mat: Dielectric(indexOfRefraction: -3/2))
+		Sphere([0, 1, -1.5], 0.5, mat: Metal(albedo: [0.2, 0.2, 0.2], fuzz: 0))
 
     ]
 )
