585ec352235b6f774e6385b51bda31023e3eacaf
Mukul Agarwal <agarmukul23@gmail.com>
2022-04-08 19:54:38 -0500

    create Material class

---
+++ Sources/RayTracer/Material.swift
@@ -0,0 +1,14 @@
+//
+//  Material.swift
+//  
+//
+//  Created by Mukul Agarwal on 4/8/22.
+//
+
+import Foundation
+
+class Material {
+    func scatter(rayIn: Ray, hitRecord: HitRecord, attenuation: Vector) -> Ray {
+        fatalError("scattering of rays must be overriden for each material")
+    }
+}
