e50d3c22db30552e38e142add6bcf08defa272fc
Mukul Agarwal <agarmukul23@gmail.com>
2022-04-09 09:06:14 -0500

    add Metal and Lambertian Materials

---
+++ Sources/RayTracer/Lambertian.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mukul Agarwal on 4/8/22.
+//
+
+import Foundation
+
+class Lambertian: Material {
+    let albedo: Color
+    init(albedo: Color) {
+        self.albedo = albedo
+    }
+    override func scatter(hitRecord: HitRecord) -> (attenuation: Color, Ray?) {
+        var scatterDirection = hitRecord.normal + Vector.randomOnUnitSphere()
+        if scatterDirection.nearZero() {
+            scatterDirection = hitRecord.normal
+        }
+        let scattered = hitRecord.p » scatterDirection
+        return (attenuation: albedo, Double.random(in:0...1) < 0.5 ? scattered : nil)
+    }
+}
--- Sources/RayTracer/Metal.swift
+++ Sources/RayTracer/Metal.swift
@@ -1,8 +1,22 @@
 //
-//  File.swift
+//  Metal.swift
 //  
 //
 //  Created by Mukul Agarwal on 4/8/22.
 //
 
 import Foundation
+
+class Metal: Material {
+    let albedo: Color
+    let fuzz: Double
+    init(albedo: Color, fuzz: Double) {
+        self.albedo = albedo
+        self.fuzz = fuzz.clamp(to: 0...1)
+    }
+    override func scatter(hitRecord: HitRecord) -> (attenuation: Color, Ray?) {
+        let reflected = hitRecord.ray.direction.normalized().reflect(normalTo: hitRecord.normal) + fuzz * Vector.randomInUnitSphere()
+        let reflects = (reflected • hitRecord.normal) > 0
+        return (attenuation: albedo, reflects ? (hitRecord.p » reflected) : nil)
+    }
+}
