563b09ed387206b6749e7a37d4dbbf87ec16adcd
Mukul Agarwal <agarmukul23@gmail.com>
2022-05-06 10:22:25 -0500

    add comments to CheckerTexture

--- Sources/RayTracer/CheckerTexture.swift
+++ Sources/RayTracer/CheckerTexture.swift
@@ -6,6 +6,8 @@ class CheckerTexture: Texture {
     let hor: Double
     let ver: Double
     let surface: Bool
+    // original initializer - creates checkered texture that alternates between two textures
+    // this uses surface (u,v) coordinates to create renderings
     init(_ a: Texture, _ b: Texture, hor: Double, ver: Double) {
         self.a = a
         self.b = b
@@ -13,6 +15,7 @@ class CheckerTexture: Texture {
         self.ver = Double.pi * ver
         self.surface = true
     }
+    // several 'convenience' initializers that allow one to pass a Color directly instead of a SolidTexture() wrapper around it
     convenience init(_ a: Color, _ b: Texture, hor: Double, ver: Double) {
         self.init(SolidTexture(color: a), b, hor: hor, ver: ver)
     }
@@ -22,6 +25,7 @@ class CheckerTexture: Texture {
     convenience init(_ a: Color, _ b: Color, hor: Double, ver: Double) {
         self.init(SolidTexture(color: a), SolidTexture(color: b), hor: hor, ver: ver)
     }
+    // original initializer - this uses the coordinates of the *point* of contact to alternate betwen the textures
     init(_ a: Texture, _ b: Texture, delta: Double) {
         self.a = a
         self.b = b
@@ -29,6 +33,7 @@ class CheckerTexture: Texture {
         self.ver = 0
         self.surface = false
     }
+    // several 'convenience' initializers that allow one to pass a Color directly instead of a SolidTexture() wrapper around it
     convenience init(_ a: Color, _ b: Texture, delta: Double) {
         self.init(SolidTexture(color: a), b, delta: delta)
     }
@@ -39,11 +44,16 @@ class CheckerTexture: Texture {
         self.init(SolidTexture(color: a), SolidTexture(color: b), delta: delta)
     }
     override func value(_ hitRecord: HitRecord) -> Color {
+        // determine which texture to use, then call its value() function
         let sines: Double
         if surface {
+            // use surface coordinates to create rendering
             let (u,v) = hitRecord.surfaceCoordinates
+            // multiply by 'hor' and 'ver' to scale by direction
             sines = sin(hor*u) * sin(ver*v)
         } else {
+            // use contact point
+            // only 'hor' is used as the scaling factor, since it is equal in all directions
             sines = sin(hor*hitRecord.p.x) * sin(hor*hitRecord.p.y) * sin(hor*hitRecord.p.z)
         }
         return sines < 0 ? a.value(hitRecord) : b.value(hitRecord)
